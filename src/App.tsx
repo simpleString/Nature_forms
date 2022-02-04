e433b38766df11a1c30e0d44372a084320edddf3
Dmitry Volkov <vdmitry.log@gmail.com>
2022-02-04 15:57:43 +0700

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,78 @@
-import React from "react";
-import "./App.css";
-import { Login } from "./components/Login";
-import { Nav } from "./components/Nav";
-import { TheoryList } from "./components/TheoryList";
+import React, { useEffect, useState } from 'react';
+import './styles/App.css';
+import { Navigation } from './components/Navigation';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
+import { PostList } from './pages/PostList';
+import { Post } from './pages/Post';
+import { NoMatch } from './pages/NoMatch';
+import { TestList } from './pages/TestList';
+import { usePosts } from './hooks/usePosts';
+import { useRecoilState } from 'recoil';
+import { authAtom, userAtom } from './state';
+import api from './utils/api';
+import { BASE_URL } from './configs';
+import { RequireAuth } from './components/RequireAuth';
+import { Spinner } from './components/UI/Spinner';
 
 function App() {
-    return (
-        <div>
-            <Nav />
-            <TheoryList />
+  const [getPosts, getPostById] = usePosts();
+
+  const [auth, setAuth] = useRecoilState(authAtom);
+  const [user, setUser] = useRecoilState(userAtom);
+  const [loading, setLoading] = useState(true);
+  const getUsername = async () => {
+    const result = await api.get(BASE_URL + 'auth');
+    if ((result.status = 200)) {
+      setAuth(true);
+      setUser(result.data.username);
+      console.log(result.data);
+    }
+  };
+
+  useEffect(() => {
+    getUsername();
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Navigation />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="main">
+          <Routes>
+            <Route
+              path="posts"
+              element={
+                <RequireAuth>
+                  <PostList />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="posts/:id"
+              element={
+                <RequireAuth>
+                  <Post />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="posts/:id/test"
+              element={
+                <RequireAuth>
+                  <TestList />
+                </RequireAuth>
+              }
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
         </div>
-    );
+      )}
+    </BrowserRouter>
+  );
 }
 
 export default App;
